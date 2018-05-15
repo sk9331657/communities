b9a3b05ae84b274e470b52778a72445b0c5c5029
anumehaagrawal <anuzenith29@gmail.com>
2018-05-15 14:19:20 +0530

    Routing for navbar completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,34 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { EventsComponent } from './events/events.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './/app-routing.module';
+import { CommunitiesComponent } from './communities/communities.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    ContactComponent,
+    CalendarComponent,
+    EventsComponent,
+    CommunitiesComponent
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot(),
+    
   ],
   providers: [],
   bootstrap: [AppComponent],
+  exports :[ RouterModule ],
   schemas: [ NO_ERRORS_SCHEMA ]
 })
 export class AppModule { }
