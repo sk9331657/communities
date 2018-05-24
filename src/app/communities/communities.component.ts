eae9ebe24b5737f14ab440b32bcdd6e5a7c981a1
anumehaagrawal <anuzenith29@gmail.com>
2018-05-24 18:22:04 +0530

    Communities page data added

--- src/app/communities/communities.component.ts
+++ src/app/communities/communities.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { COMM } from '../commun-data';
 @Component({
   selector: 'app-communities',
   templateUrl: './communities.component.html',
   styleUrls: ['./communities.component.scss']
 })
 export class CommunitiesComponent implements OnInit {
+   public myInterval: number = 300;
+    public activeSlideIndex: number = 0;
+    public noWrapSlides:boolean = false;
+
+    activeSlideChange(){
+        console.log(this.activeSlideIndex);
+    }
+
+    public slides:Array<Object> = [
+        {"image":"../assets/img/women2.jpg"},
+        {"image":"../assets/img/women1.jpg"},
+        {"image":"../assets/img/women3.jpg"},
+    ];
+    details = COMM;
+
 
   constructor() { }
 
