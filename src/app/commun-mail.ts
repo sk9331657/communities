32764343aa6d6f3272913ab69ab942f6789af073
anumehaagrawal <anuzenith29@gmail.com>
2018-05-30 23:30:04 +0530

    Communities page completed

---
+++ src/app/commun-mail.ts
@@ -0,0 +1,17 @@
+import { Commun } from './communty';
+export const MAILG: Commun[] = [
+   { title: 'Ph.D.JobHuntHers', mailing: 'http://systers.org/mailman/listinfo/phdjobhunthers', desc:'This moderated mailing list supports women who are seeking or beginning doctorate-level jobs in computer science computer engineering or information technology either in academia industry or government laboratories.', img:'' },
+
+  { title: 'ProfessHers', mailing: 'http://systers.org/mailman/listinfo/professhers', desc:'This moderated mailing list provides a venue for  women faculty members in computer science computer engineering and information technology to informally discuss job-related issues and career advancement.', img:'' },
+
+  { title: 'JrProfessHers', mailing: 'http://systers.org/mailman/listinfo/pretenures', desc:' JrProfessHers is a moderated mailing list for women faculty in computer science computer engineering and information technology who do not yet have tenure. ', img:'' },
+
+  { title: 'Student-ResearcHers', mailing: 'http://systers.org/mailman/listinfo/student-researchers', desc:' Systers involved in IETF topics can participate by email or in person at Systers-IETF meetings The purpose of these gatherings is to give us a chance to socialize with other women who attend the IETF and to offer mutual support in our IETF activities. ', img:'' },
+
+  { title: 'ResearcHers', mailing: 'http://systers.org/mailman/listinfo/researchers', desc:'This informal organization gathers non-managerial non-academic female computer science researchers.', img:'' }
+
+
+
+
+];
+  
---
+++ src/app/commun-sig.ts
@@ -0,0 +1,20 @@
+
+import { Commun } from './communty';
+export const SIG: Commun[] = [
+  { title: 'Entrepreneurs', mailing: 'http://systers.org/mailman/listinfo/entrepreneurs', desc:'Share experiences and discuss questions of interest to female entrepreneurs in IT and related areas', img:'' },
+
+  { title: 'Systers-CommColl', mailing: 'http://www.systers.org/mailman/listinfo/systers-commcoll ', desc:'Join a a private email forum for female technical students alumnae faculty and staff at community colleges. ', img:'' },
+
+  { title: 'Systers-IETF', mailing: 'https://www.ietf.org/mailman/listinfo/systers', desc:' The purpose of these gatherings is to give us a chance to socialize with other women who attend the IETF and to offer mutual support in our IETF activities. ', img:'' },
+
+  { title: 'Student-ResearcHers', mailing: 'http://systers.org/mailman/listinfo/student-researchers', desc:' The purpose of these gatherings is to give us a chance to socialize with other women who attend the IETF and to offer mutual support in our IETF activities. ', img:'' },
+
+  { title: 'Systers-TechTalk', mailing: 'http://systers.org/mailman/listinfo/systers-techtalk', desc:'Systers-Techtalk is a private email forum for technical discussions by women who are university students or working in the technical side of computing Systers-Techtalk follows the rules and guidelines dictated by Systers FAQs ', img:'' },
+
+  { title: 'Systers-Wikipedia', mailing: 'http://systers.org/mailman/listinfo/systers-wikipedia', desc:'Join a list for female Wikipedians.', img:'' },
+
+  { title: ' Women in Cybersecurity & Privacy', mailing: 'https://www.wicys.net/', desc:'Join women from academia research and industry to network share knowledge and provide or receive mentoring in cybersecurity.', img:'' }
+  
+  
+];
+
